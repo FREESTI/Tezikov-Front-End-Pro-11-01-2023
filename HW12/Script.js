d635d6b8b6513c25f8166392c0644a05021357b4
FREESTI <115978429+FREESTI@users.noreply.github.com>
2023-01-25 21:31:18 +0200

    Update splice

--- HW12/Script.js
+++ HW12/Script.js
@@ -1,5 +1,5 @@
 const arr = [4, 1, -3, 2, 6, 3, 5, 9, 7, 8]
-arr.splice(2, 4); 
+arr.splice(1, 3); 
 arr.sort((a, b) => a - b)
 console.log( arr );
 
@@ -7,5 +7,5 @@ console.log( arr );
 
 const nameUsers = ['Michael', 'William ', 'Liam', 'Alexander', 'Jacob', 'Mason', 'Jack',]
 const nameSort = nameUsers.sort();
-nameSort.splice(2, 4); 
+nameSort.splice(1, 3); 
 console.log(nameSort);
