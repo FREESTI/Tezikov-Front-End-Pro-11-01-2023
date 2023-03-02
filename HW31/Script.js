8a47528328c471cfb714735dd0d2966def0b8b48
FREESTI <115978429+FREESTI@users.noreply.github.com>
2023-03-03 00:41:17 +0200

    Update Script.js

--- HW31/Script.js
+++ HW31/Script.js
@@ -1,48 +1,48 @@
-class SuperMath {
-  check(obj) {
-    const x = obj.X;
-    const y = obj.Y;
-    let znak = obj.znak;
-    const validOperators = ["+", "-", "*", "/", "%"];
-    
-    if (!validOperators.includes(znak)) {
-      while (!validOperators.includes(znak)) {
-        znak = prompt(`Помилка: неправильний оператор. Будь ласка, введіть правильний оператор (${validOperators.join(", ")}):`);
-      }
-    }
-    
-    let result;
-    const performOperation = confirm(`Бажаєте виконати операцію ${x} ${znak} ${y}?`);
-    
-    if (performOperation) {
-      switch (znak) {
-        case "+":
-          result = x + y;
-          break;
-        case "-":
-          result = x - y;
-          break;
-        case "*":
-          result = x * y;
-          break;
-        case "/":
-          result = x / y;
-          break;
-        case "%":
-          result = x % y;
-          break;
-      }
-      alert(`Результат: ${result}`);
-    } else {
-      this.check({ X: parseInt(newX), Y: parseInt(newY), znak: newZnak });
-    }
+const arr = [1, 1, 2, 3, 4, 5, 5, 5, 6];
+const uniqueArr = arr.filter((elem, index, array) => {
+  return array.indexOf(elem) === index;
+});
+console.log(uniqueArr);
+
+
+
+
+const bob = {
+  name: "Bob",
+};
+const jane = {
+  name: "Jane",
+  kids: ["Tolik", "Vasya"],
+};
+const GreetMap = new Map();
+GreetMap.set(bob, `Hello ${bob.name} nice to see you`);
+GreetMap.set(jane, `How your kids doing ${jane.name}?`);
+
+console.log(GreetMap.get(bob));
+console.log(GreetMap.get(jane));
+
+function convert(data) {
+  if (data instanceof Map) {
+    return Object.fromEntries(data);
+  } else if (typeof data === "object" && data !== null) {
+    return new Map(Object.entries(data));
+  } else {
+    throw new Error("ERROR");
   }
 }
-const p = new SuperMath();
 
-const x = prompt("Введите значение X:");
-const y = prompt("Введите значение Y:");
-let znak = prompt("Введите знак (+, -, *, / или %):");
 
-const obj = { X: parseInt(x), Y: parseInt(y), znak: znak };
-p.check(obj);
+
+
+const object = {
+  a: 1,
+  b: 2,
+  с: 3,
+};
+const map = new Map(Object.entries(object));
+
+const checkMap = convert(object);
+console.log(checkMap instanceof Map);
+
+const chekObj = convert(map);
+console.log(typeof chekObj);
