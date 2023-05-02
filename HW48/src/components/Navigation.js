81367855a76038f6395d905a98d6cb0752f896ce
FREESTI <115978429+FREESTI@users.noreply.github.com>
2023-05-02 13:53:37 +0300

    Add files via upload

---
+++ HW48/src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+			<li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=1s">1</a></li>
+      <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=1s">2</a></li>
+      <li><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ&t=1s">3</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
