3fa3254d301ca95033b1dbbf08d311afd2895c69
FREESTI <115978429+FREESTI@users.noreply.github.com>
2023-05-22 02:49:59 +0300

    Add files via upload

---
+++ formik/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from './components/Form'; // Обновленный путь импорта
+
+const App = () => {
+  return (
+    <div>
+      <h1>Форма</h1>
+      <Form />
+    </div>
+  );
+};
+
+export default App;
