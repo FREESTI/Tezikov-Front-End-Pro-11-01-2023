2c540accbfecad3f3e0a7e5ee7ca2cfd34ea98f4
FREESTI <115978429+FREESTI@users.noreply.github.com>
2023-05-24 11:47:32 +0300

    Update App.js

--- TODOLIST/src/App.js
+++ TODOLIST/src/App.js
@@ -1,33 +1,32 @@
-import React, { useState } from 'react';
-import TodoList from './components/TodoList';
-import TodoForm from './components/TodoForm';
-import './style/app.css';
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { addTodo, toggleTodo } from "./actions";
+import TodoList from "./components/TodoList";
+import TodoForm from "./components/TodoForm";
+import "./style/app.css";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, text: "пам пам", completed: false },
-    { id: 2, text: "бум бум", completed: true },
-    { id: 3, text: "гав гав", completed: false }
-  ]);
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+  const [text, setText] = useState("");
 
-  const toggleTodo = (id) => {
-    setTodos(
-      todos.map((todo) =>
-        todo.id === id ? { ...todo, completed: !todo.completed } : todo
-      )
-    );
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (text.trim()) {
+      dispatch(addTodo(text.trim()));
+      setText("");
+    }
   };
 
-  const addTodo = (text) => {
-    const newTodo = { id: Date.now(), text, completed: false };
-    setTodos([...todos, newTodo]);
+  const handleTodoClick = (id) => {
+    dispatch(toggleTodo(id));
   };
 
   return (
     <div className="App">
       <h1>ТУДУШКА</h1>
-      <TodoForm addTodo={addTodo} />
-      <TodoList todos={todos} toggleTodo={toggleTodo} />
+      <TodoForm text={text} setText={setText} handleSubmit={handleSubmit} />
+      <TodoList todos={todos} handleTodoClick={handleTodoClick} />
     </div>
   );
 }
